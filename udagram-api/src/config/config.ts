e8b00d2576504de9e79453e549473bcdf4d754d5
dung-fpt <dung.us.nf@gmail.com>
2024-09-09 10:26:54 +0700

    update configuration

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'postgres',
+  'password': 'Aa123456',
+  'database': 'postgres',
+  'host': 'udacity.cfsoqes9ekvj.us-east-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'mynd9990',
+  'url': 'http://localhost:8080',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': 'testing',
   },
 };
